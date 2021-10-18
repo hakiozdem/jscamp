a9001932e68893583dbcf888f151172e787fee3f
hakiozdem <haktanozdem@gmail.com>
2021-10-19 00:05:51 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+//let
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9"
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+//constant
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<3;i++){
+    console.log("<li>"+konutKredileri[i]+"<li>")
+}
+console.log("</ul>")
